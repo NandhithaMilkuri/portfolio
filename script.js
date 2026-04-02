8cfb0e26c2fdfc71adef83135eb191932fa12ef6
MilkuriNandhitha <2303a51352@sru.edu.in>
2026-04-02 12:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// typing name
 const text="Hello, I'm Milkuri Nandhitha";
 let i=0;
 
@@ -8,5 +9,18 @@ i++;
 setTimeout(type,80);
 }
 }
-
 type();
+
+// scroll animation
+const reveals=document.querySelectorAll('.reveal');
+
+function reveal(){
+reveals.forEach(el=>{
+const top=el.getBoundingClientRect().top;
+if(top<window.innerHeight-100){
+el.classList.add('active');
+}
+});
+}
+
+window.addEventListener('scroll',reveal);
